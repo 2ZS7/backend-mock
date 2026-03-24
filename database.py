15a43ae8a08429a8de778d532457e0c650961b05
2ZS7 <szulfira79@gmail.com>
2026-03-25 03:43:39 +0500

    update database

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 import redis.asyncio as redis # Используем современный асинхронный клиент
 
 # --- MongoDB ---
-MONGO_URL = "mongodb://localhost:27017"
+MONGO_URL = "mongodb://mongodb:27017"
 mongo_client = AsyncIOMotorClient(MONGO_URL)
 db = mongo_client["mock_engine_db"]
 sessions_collection = db["sessions"]
@@ -12,6 +12,6 @@ virtual_state_collection = db["virtual_state"]
 request_logs_collection = db["request_logs"]
 
 # --- Redis ---
-REDIS_URL = "redis://localhost:6379"
+REDIS_URL = "redis://redis:6379"
 # decode_responses=True означает, что Redis будет возвращать обычные строки, а не байты
 redis_client = redis.from_url(REDIS_URL, decode_responses=True)
